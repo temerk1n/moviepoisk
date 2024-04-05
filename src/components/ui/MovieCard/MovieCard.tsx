e7d10cb0ca660b819b594cd8328775b669cc8b69
temerk1n <nikita.verigin23@gmail.com>
2024-04-05 19:07:28 +0300

    feat: change MovieCard style

--- src/components/ui/MovieCard/MovieCard.tsx
+++ src/components/ui/MovieCard/MovieCard.tsx
@@ -1,5 +1,5 @@
 import {Movie} from "../../../types/Movie";
-import {Card, Flex, Image, List, Rate} from "antd";
+import {Card, Col, Flex, Image, List, Rate, Row} from "antd";
 
 interface MovieCardProps {
   movie: Movie,
@@ -10,20 +10,28 @@ export const MovieCard = ({movie}: MovieCardProps) => {
     <List.Item
     key={movie.id}>
       <Card bordered={false} hoverable>
-        <Flex gap="middle" justify="space-between">
-          <Image width={150} src={movie.poster.previewUrl}/>
-          <Flex gap="small" vertical align="start">
-            <h2>{movie.name}</h2>
-            <span>{movie.alternativeName}, {movie.year}, {movie.movieLength} мин.</span>
-            <span>{movie.countries[0].name} {movie.genres[0].name}</span>
-            <p>{movie.shortDescription}</p>
-          </Flex>
-          {
-            movie.rating.kp ?
-            <Rate disabled allowHalf defaultValue={0} value={movie.rating.kp / 2} count={5}/> :
-            <></>
-          }
-        </Flex>
+        <Row justify="space-between">
+          <Col flex={1}>
+            <Image width={150} src={movie.poster.previewUrl}/>
+          </Col>
+          <Col flex={6}>
+            <Flex gap="small" vertical align="start">
+              <h2>{movie.name}</h2>
+              <span>{movie.alternativeName}, {movie.year}, {movie.movieLength} мин.</span>
+              <span>{movie.countries[0].name} {movie.genres[0].name}</span>
+              <p>{movie.shortDescription}</p>
+            </Flex>
+          </Col>
+          <Col flex={2}>
+            <Flex justify="flex-end">
+              {
+                movie.rating.kp ?
+                  <Rate disabled allowHalf defaultValue={0} value={movie.rating.kp / 2} count={5}/> :
+                  <></>
+              }
+            </Flex>
+          </Col>
+        </Row>
       </Card>
     </List.Item>
   )
