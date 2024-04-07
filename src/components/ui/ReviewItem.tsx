4022b1799d90d9ad6aa3c58ac0ec937df91ab6da
temerk1n <nikita.verigin23@gmail.com>
2024-04-07 19:05:28 +0300

    chore: add formatDate function

--- src/components/ui/ReviewItem.tsx
+++ src/components/ui/ReviewItem.tsx
@@ -1,6 +1,7 @@
 import {Review} from "../../types/Review";
 import {Card, List, Typography} from "antd";
 import {CSSProperties, FC} from "react";
+import {formatDate} from "../../utils/formatDate";
 
 const {Title, Text, Paragraph} = Typography;
 
@@ -17,15 +18,12 @@ interface ReviewItemProps {
 }
 
 export const ReviewItem: FC<ReviewItemProps> = ({review}) => {
-  const date = new Date(review.date);
-  const dateString = date.toLocaleDateString("ru") + " в " + date.getHours() + ":" + date.getMinutes();
-
   return (
     <List.Item key={review.id}>
       <Card
         bordered={false}
         title={<Title level={5}>{review.author}</Title>}
-        extra={<Text>{dateString}</Text>}
+        extra={<Text>{formatDate(review.date)}</Text>}
         style={review.type === "Позитивный" ? positiveReviewStyle : negativeReviewStyle}
       >
         <Title level={5}>{review.title}</Title>
