b316308d520704ca80d9da1a8ed8be4cd55606ec
temerk1n <nikita.verigin23@gmail.com>
2024-04-14 17:30:01 +0300

    fix: ReviewItem style

--- src/components/ui/ReviewItem.tsx
+++ src/components/ui/ReviewItem.tsx
@@ -1,5 +1,5 @@
 import { Review } from "../../types/Review";
-import { Card, List, Typography } from "antd";
+import { Card, Flex, List, Typography } from "antd";
 import { CSSProperties, FC } from "react";
 import { formatDate } from "../../utils/formatDate";
 
@@ -22,16 +22,20 @@ export const ReviewItem: FC<ReviewItemProps> = ({ review }) => {
     <List.Item key={review.id}>
       <Card
         bordered={false}
-        title={<Title level={5}>{review.author}</Title>}
-        extra={<Text>{formatDate(review.date)}</Text>}
         style={
           review.type === "Позитивный"
             ? positiveReviewStyle
             : negativeReviewStyle
         }
       >
-        <Title level={5}>{review.title}</Title>
-        <Paragraph>{review.review}</Paragraph>
+        <Flex align="center" justify="space-between">
+          <Title level={4}>{review.author}</Title>
+          <Text>{formatDate(review.date)}</Text>
+        </Flex>
+        <Flex vertical>
+          <Title level={5}>{review.title}</Title>
+          <Paragraph>{review.review}</Paragraph>
+        </Flex>
       </Card>
     </List.Item>
   );
